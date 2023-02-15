8bcb3e663dddd64f97862c10feda9db12c754df8
RAKIB <rakibmahmudmridha@gmail.com>
2023-02-15 17:11:18 +0600

    probelm solved

---
+++ Prefix Sum/print_left_element.cpp
@@ -0,0 +1,31 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+    public:
+    int leftElement(int a[], int n) {
+    sort(a,a+n);
+        if(n%2==0)
+        return a[n/2 - 1];
+        return a[n/2];
+    }
+
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int a[n], i;
+        for (i = 0; i < n; i++) cin >> a[i];
+        Solution ob;
+        cout << ob.leftElement(a, n) << endl;
+    }
+}
+
+// } Driver Code Ends

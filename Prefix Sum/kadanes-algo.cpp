2c1b95a0dc9b409ef9999a434659180f4d86d69a
RAKIB <rakibmahmudmridha@gmail.com>
2023-02-15 09:03:14 +0600

    file modified

--- Prefix Sum/kadanes-algo.cpp
+++ Prefix Sum/kadanes-algo.cpp
@@ -4,31 +4,31 @@ using namespace std;
 
 int maxSubArraySum(int a[], int size)
 {
-    int maxSum = INT_MIN, maxEnd = 0;
-    for ( int i = 0; i < size; i++ )
+    int maxSubarraySum(int arr[], int n)
     {
-        maxEnd +=a[i];
-        if( maxEnd > maxSum )
+        int max_so_far = INT_MIN;
+        int max_ending_here = 0;
+
+        for (int i = 0; i < n; i++)
         {
-            maxSum = maxEnd;
+            max_ending_here = max(max_ending_here + arr[i], arr[i]);
+            max_so_far = max(max_so_far, max_ending_here);
         }
 
-        if( maxEnd < 0 )
-            maxEnd = 0;
-
+        return max_so_far;
     }
-    return maxSum;
+
 }
 
 // Driver Code
 int main()
 {
-	int a[] = { -2, -3, 4, -1, -2, 1, 5, -3 };
-	int n = sizeof(a) / sizeof(a[0]);
+    int a[] = { -2, -3, 4, -1, -2, 1, 5, -3 };
+    int n = sizeof(a) / sizeof(a[0]);
 
-	// Function Call
-	int max_sum = maxSubArraySum(a, n);
-	cout << "Maximum contiguous sum is " << max_sum;
-	return 0;
+    // Function Call
+    int max_sum = maxSubArraySum(a, n);
+    cout << "Maximum contiguous sum is " << max_sum;
+    return 0;
 }
 

08a6c691fbf859b64046ff8f992e80ffb09f7b8f
RAKIB <rakibmahmudmridha@gmail.com>
2023-02-16 10:48:28 +0600

    prefix sum

---
+++ Prefix Sum/maxPrefixSum.cpp
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+// } Driver Code Ends
+class Solution
+{
+public:
+    vector<int> maxPrefixes(int a[], int L[], int R[], int N, int Q)
+    {
+
+      vector<int> result;
+
+      for( int i = 0; i < Q; i++ )
+      {
+          int maxValue = INT_MIN;
+          int pre = 0;
+
+          for( int j = L[i]; j <= L[R]; j++ )
+          {
+              pre += a[j];
+              maxValue = max(maxValue, pre);
+          }
+          v.push_back(maxValue);
+      }
+
+        return result;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, q;
+        cin >> n >> q;
+        int a[n], L[q], R[q];
+
+        for (int i = 0; i < n; i++) cin >> a[i];
+
+        for (int i = 0; i < q; i++) cin >> L[i] >> R[i];
+        Solution obj;
+        vector<int> ans = obj.maxPrefixes(a, L, R, n, q);
+        for (auto it : ans)
+            cout << it << " ";
+        cout << endl;
+    }
+}
+
+// } Driver Code Ends

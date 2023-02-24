78c3bfbf60ab4b3e2637ea8045242f3623c0e9e2
RAKIB <rakibmahmudmridha@gmail.com>
2023-02-24 15:30:23 +0600

    common divisors

--- CSES/Math/commonDIvisors.cpp
+++ CSES/Math/commonDIvisors.cpp
@@ -4,24 +4,33 @@ using namespace std;
 #define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 #define ll long long
 
-ll gcd( ll a, ll b )
-{
-    if(b == 0)
+int gcd(int a, int b) {
+    if (b == 0) {
         return a;
+    }
     return gcd(b, a % b);
 }
 
+int maxGcd(int arr[], int n) {
+    int max_gcd = 0;
 
-ll maxGCD( ll arr[], ll n )
-{
-    ll max_gcd = 0;
+    // Find the maximum value in the array
+    int max_val = *max_element(arr, arr + n);
 
-    for( ll i = 0; i < n; i++ )
-    {
-        for( ll j = i + 1; j < n; j++ )
-        {
-            ll current_gcd = gcd(arr[i], arr[j]);
-            max_gcd = max( max_gcd, current_gcd );
+    // Count the number of occurrences of each value from 1 to max_val
+    int count[max_val + 1] = {0};
+    for (int i = 0; i < n; i++) {
+        count[arr[i]]++;
+    }
+
+    // Compute the greatest common divisor for all multiples of each value
+    for (int i = 1; i <= max_val; i++) {
+        int divisors = 0;
+        for (int j = i; j <= max_val; j += i) {
+            divisors += count[j];
+        }
+        if (divisors > 1) {
+            max_gcd = max(max_gcd, i);
         }
     }
 
@@ -32,15 +41,17 @@ ll maxGCD( ll arr[], ll n )
 int main() {
     optimize();
 
-    ll n;
+    int n;
     cin >> n;
 
-    ll arr[n];
-    for( ll i = 0; i < n; i++ ) cin >> arr[i];
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
 
-    ll max_gcd = maxGCD( arr, n );
+    int max_gcd = maxGcd(arr, n);
+
     cout << max_gcd << endl;
-
     return 0;
 }
 

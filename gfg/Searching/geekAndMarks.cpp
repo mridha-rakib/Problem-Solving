45530a9e218723f831ac18653dc9d522c23d0f51
RAKIB <rakibmahmudmridha@gmail.com>
2023-02-28 09:57:10 +0600

    liniear search

--- gfg/Searching/geekAndMarks.cpp
+++ gfg/Searching/geekAndMarks.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+	//code
+	int t;
+	cin >> t;
+	while( t-- )
+    {
+      int n, x;
+      cin >> n >> x;
+
+      int arr[n];
+      for(int i = 0; i < n; i++ ) cin >> arr[i];
+
+      int count = 0;
+      for( int i = 0; i < n; i++ )
+      {
+          if(arr[i] > x ) count++;
+      }
+
+      cout << count << endl;
+    }
+
+	return 0;
+}

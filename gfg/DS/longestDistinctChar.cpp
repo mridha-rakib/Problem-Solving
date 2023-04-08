1f7b227b60bf289c88bf9fe415dcdb6a5ab3982e
RAKIB <rakibmahmudmridha@gmail.com>
2023-04-08 17:38:09 +0600

    ds algo

---
+++ gfg/DS/longestDistinctChar.cpp
@@ -0,0 +1,27 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+int longestSubstrDistinctChars (string S);
+int main()
+{
+    int t; cin >> t;
+    while (t--)
+    {
+        string S; cin >> S;
+
+        cout << longestSubstrDistinctChars (S) << endl;
+    }
+}
+
+// Contributed By: Pranay Bansal
+
+// } Driver Code Ends
+
+
+int longestSubstrDistinctChars (string S)
+{
+    // your code here
+    sort( S.begin(), S.end() );
+    int sz = unique( S.begin(), S.end()) - S.begin();
+    return sz;
+}

3c47b5161e0533e59c8a22d2d3ac0fb7e2c79829
RAKIB <rakibmahmudmridha@gmail.com>
2023-04-09 08:53:47 +0600

    stack & queue ds

---
+++ gfg/DS/SearchElement.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    // Function to search x in arr
+    // arr: input array
+    // X: element to be searched for
+    int search(int arr[], int N, int X)
+    {
+
+        // Your code here
+
+        auto it = find(arr, arr + N, X);
+
+        if (it != arr + N)
+            return it - arr;
+        return -1;
+    }
+
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int testcases;
+    cin>>testcases;
+    while(testcases--)
+    {
+        int sizeOfArray;
+        cin>>sizeOfArray;
+        int arr[sizeOfArray];
+        int x;
+
+        for(int i=0;i<sizeOfArray;i++)
+        {
+            cin>>arr[i];
+        }
+        cin>>x;
+        Solution ob;
+        cout<<ob.search(arr,sizeOfArray,x)<<endl; //Linear search
+    }
+
+    return 0;
+
+}
+
+// } Driver Code Ends
--- gfg/DS/longestDistinctChar.cpp
+++ gfg/DS/longestDistinctChar.cpp
@@ -21,7 +21,19 @@ int main()
 int longestSubstrDistinctChars (string S)
 {
     // your code here
-    sort( S.begin(), S.end() );
-    int sz = unique( S.begin(), S.end()) - S.begin();
-    return sz;
+    int cnt = 0;
+    int ans = 0;
+    for ( int i = 0; i < S.size() - 1; i++ )
+    {
+        if ( S[i] != S[i + 1] )
+        {
+            cnt++;
+        }
+        else
+        {
+            ans = max ( cnt, ans );
+            cnt = 0;
+        }
+    }
+    return ans;
 }
